d62d351c43e8b715b176159c6b26750a514770cc
Breno-Pimentel <brenopimentel.a.m@outlook.com>
2023-09-14 11:36:44 -0300

    Condições de existencia da tabela e do banco de dados - adiconado

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,41 +1,62 @@
-const { Pool, Client } = require('pg');
+//Importando classes
+const { Pool, Client } = require("pg");
+const path = require("path");
+require("dotenv").config({ debug: true });
 
- const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: '240403breno',
-    port: 5432
-})
+//Pool inicial
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+});
 
-//Função para conectar o banco de daddos
-const execute = async (query) => {
-
-    try {
-        await pool.connect();
-        await pool.query(query);
-        return true;
-    } catch (error) {
-        console.error(error.stack);
-        return false;
-    } finally {
-        await pool.end();
-    }
-}
-//Constante responsável por injetar o script sql no postgres
-const text = `
-CREATE TABLE IF NOT EXISTS "users" (
+//Scripts do banco de dados
+const createDbScript = "CREATE DATABASE db_login_system";
+const createTBScript = `CREATE TABLE IF NOT EXISTS "users" (
     "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR(100) NOT NULL,
-    "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL
+    "name" VARCHAR(55) NOT NULL,
+    "email" VARCHAR(55) UNIQUE NOT NULL,
+    "password" VARCHAR(100) NOT NULL
 );`;
 
-//Executando o metodo passando a const text como parametro
-execute(text).then(result => {
-    if(result) {
-        console.log('Table created');
-}})
+//Função que cria o banco de daddos e a tabela do projeto(Utilizando verficações de existencia);
+const startDatabase = async () => {
+  //Try(tente executar isso) e catch(Caso não funcione retorne um erro) da função
+  try {
+    //Script de verificação da existencia do banco de dados
+    let dataBaseVerify = await pool.query(
+      "SELECT datname FROM pg_catalog.pg_database WHERE LOWER(pg_database.datname) = LOWER('db_login_system')"
+    );
+    //Condição para que o banco de dados seja criado
+    if (dataBaseVerify.rows.length == 0) {
+      await pool.query(createDbScript);
+      console.info("Database created successfully");
+    }
+    //Pool principal, que se conecta ao banco de dados de usuários
+    const mainPool = new Pool({
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_MAIN_NAME,
+    });
+    //Script de verificação da existencia da tabela 'users'
+    let tableVerify = await pool.query(
+      "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE LOWER(TABLE_NAME) = LOWER('users')"
+    );
+    //Condição para que a tabela 'users' seja criada
+    if (tableVerify.rows.length == 0) {
+      await mainPool.query(createTBScript);
+      console.info("Table created successfully");
+    }
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
+//Execução da funcão
+startDatabase();
 
 //exportando o metodo pool para fora do arquivo
-module.exports = pool;
+module.exports = { pool, startDatabase };
