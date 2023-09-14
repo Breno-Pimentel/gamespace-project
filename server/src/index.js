d50fbc996c0cab368a527e82076c4b7e58882497
Breno-Pimentel <brenopimentel.a.m@outlook.com>
2023-09-14 14:46:58 -0300

    Banco de dados inciando automaticamente

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-
-
+require("dotenv").config({ override: true });
+const startDatabase = require("./config/database");
 
 //middlewares - faz referencias a funcoes que são passads antes da rota
 app.use(express.json());
@@ -10,8 +10,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 //rotas
-app.use(require('./routes/routes'))
+app.use(require("./routes/routes"));
 
-
-app.listen(3000);
-console.log('listening on port 3000');
+app.listen(process.env.PORT);
+console.log("listening on port 3000");
+console.log("Server is running ok");
