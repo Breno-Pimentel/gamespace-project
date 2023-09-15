7032cbdfebd4bdd0c11e09a8e2e7d758bc061893
Breno-Pimentel <brenopimentel.a.m@outlook.com>
2023-09-15 16:01:04 -0300

    Verificação de senha a nivel de frontend 80%

---
+++ client/js/index.js
@@ -0,0 +1,46 @@
+//##################################
+//            Variáveis
+//##################################
+const btn = document.getElementById("btn");
+const registerForm = document.getElementById("register-form");
+const emailInput = document.querySelector(".email-user-input");
+const passwordInput = document.querySelector("#password-user-input");
+const passwordConfirm = document.querySelector("#passsword-isEquals");
+
+//##################################
+//         FUNÇOES E EVENTOS
+//##################################
+
+//Evento do botão que recupera os valores do formulário e envia para o banco de dados
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  function isPasswordTheSame() {
+    if (passwordInput.value === passwordConfirm.value) {
+      alert("As senhas são iguais");
+    } else {
+      console.log("As senhas são diferentes");
+    }
+  }
+  isPasswordTheSame();
+  //Instanciando a clase formData
+  const formaData = new FormData(registerForm);
+
+  //trasnforma tudo em um objeto(JSON)
+  const data = Object.fromEntries(formaData);
+
+  //Constante que armazena a função assincrona de fetch
+  const fetchAPI = async () => {
+    const result = await fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
+  //Execução do Fetch(Busca)
+});
