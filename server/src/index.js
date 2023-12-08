143abb5ae88e28110cdb3dd26d55cdea9cf6ac7f
RoqueLeto <roqueleto@gmail.com>
2023-12-07 23:18:55 -0300

    RO007

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-app.use(cors());
 const app = express();
 require("dotenv").config({ override: true });
 const startDatabase = require("./database/userDatabase");
