91740c9de846d9debfce5735607258b2803003d6
RoqueLeto <roqueleto@gmail.com>
2023-12-08 16:08:22 -0300

    RO024

--- server/src/index.js
+++ server/src/index.js
@@ -1,31 +1,3 @@
-/*
-const express = require("express");
-const cors = require("cors");
-const app = express();
-require("dotenv").config({ override: true });
-const startDatabase = require("./database/userDatabase");
-const createGamesTable = require("./database/gamesDatabase");
-const createGamespaceTable = require("./database/gamespaceDatabase");
-
-//middlewares - faz referencias a funcoes que são passadas antes da rota
-app.use(cors({
-  origins: ["http://localhost", "https://localhost"],
-  }));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-
-//rotas
-app.use(require("./routes/routes"));
-
-app.listen(3000);
-console.log("listening on port 3000");
-console.log("Server is running ok");
-
-//Breno é um lindão que joga duro na kofre futuro engenheiro
-//Roque barrigudo
-//Roque, o serasa me ligou hoje
-*/
-
 const express = require("express");
 const axios = require("axios");
 const app = express();
@@ -63,3 +35,35 @@ app.get("/api/users/:id", async (req, res) => {
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
+
+
+
+
+/*
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config({ override: true });
+const startDatabase = require("./database/userDatabase");
+const createGamesTable = require("./database/gamesDatabase");
+const createGamespaceTable = require("./database/gamespaceDatabase");
+
+//middlewares - faz referencias a funcoes que são passadas antes da rota
+app.use(cors({
+  origins: ["http://localhost", "https://localhost"],
+  }));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+
+//rotas
+app.use(require("./routes/routes"));
+
+app.listen(3000);
+console.log("listening on port 3000");
+console.log("Server is running ok");
+
+//Breno é um lindão que joga duro na kofre futuro engenheiro
+//Roque barrigudo
+//Roque, o serasa me ligou hoje
+*/
+
--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,48 +1,3 @@
-/*
-// routes.js
-const { Router } = require("express");
-const router = Router();
-const cors = require("cors");
-
-// Importando o middleware de upload
-const upload = require("../middleware/MulterMiddleware");
-
-// Importando as funções da classe Controller
-const {
-  getUsers,
-  createUser,
-  getUserByID,
-  deleteUser,
-  updateUser,
-} = require("../controller/userController");
-const {
-  getGames,
-  createGame,
-  getGameByID,
-} = require("../controller/gameController");
-const loginUser = require("../controller/LoginController");
-
-// Rotas Públicas
-router.get("/users", getUsers);
-router.post("/auth/register", createUser);
-router.delete("/users/:id", deleteUser);
-router.put("/users/:id", updateUser);
-router.post("/auth/login", loginUser);
-
-router.get("/games", getGames);
-router.get("/game/:id", getGameByID);
-router.post("/upload", upload.single("img"), (req, res) => {
-  // Lógica para tratar o upload da imagem
-  res.json({ message: "Image uploaded successfully", filename: req.file.filename });
-});
-router.post("/create/game", createGame);
-
-// Rotas Privadas
-router.get("/user/:id", getUserByID);
-
-// Exportando o método router para fora do arquivo
-module.exports = router;
-*/
 // routes.js
 const { Router } = require("express");
 const router = Router();
@@ -99,3 +54,50 @@ router.get("/user/:id", async (req, res) => {
 module.exports = router;
 
 
+
+
+/*
+// routes.js
+const { Router } = require("express");
+const router = Router();
+const cors = require("cors");
+
+// Importando o middleware de upload
+const upload = require("../middleware/MulterMiddleware");
+
+// Importando as funções da classe Controller
+const {
+  getUsers,
+  createUser,
+  getUserByID,
+  deleteUser,
+  updateUser,
+} = require("../controller/userController");
+const {
+  getGames,
+  createGame,
+  getGameByID,
+} = require("../controller/gameController");
+const loginUser = require("../controller/LoginController");
+
+// Rotas Públicas
+router.get("/users", getUsers);
+router.post("/auth/register", createUser);
+router.delete("/users/:id", deleteUser);
+router.put("/users/:id", updateUser);
+router.post("/auth/login", loginUser);
+
+router.get("/games", getGames);
+router.get("/game/:id", getGameByID);
+router.post("/upload", upload.single("img"), (req, res) => {
+  // Lógica para tratar o upload da imagem
+  res.json({ message: "Image uploaded successfully", filename: req.file.filename });
+});
+router.post("/create/game", createGame);
+
+// Rotas Privadas
+router.get("/user/:id", getUserByID);
+
+// Exportando o método router para fora do arquivo
+module.exports = router;
+*/
