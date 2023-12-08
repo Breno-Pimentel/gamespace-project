feb3ac85f38b6f1e47098e0638ee84fddd00e114
RoqueLeto <roqueleto@gmail.com>
2023-12-08 10:40:14 -0300

    RO016

--- server/src/index.js
+++ server/src/index.js
@@ -1,38 +1,33 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require("dotenv").config({ override: true });
+const dotenv = require("dotenv");
+const startDatabase = require("./database/userDatabase");
+const createGamesTable = require("./database/gamesDatabase");
+const createGamespaceTable = require("./database/gamespaceDatabase");
 
-// Database initialization
-const { startDatabase, createGamesTable, createGamespaceTable } = require("./database");
+dotenv.config({ override: true });
+
+// Constantes
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "localhost";
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Routes
-const routes = require("./routes/routes");
-app.use(routes);
+// Rotas
+app.use(require("./routes/routes"));
 
-// Start server
-const port = process.env.PORT || 3000;
-
-startDatabase()
-  .then(() => createGamesTable())
-  .then(() => createGamespaceTable())
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Server is listening on port ${port}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Error starting server:", error);
-  });
-
-
-
-
+// Inicialização do servidor
+app.listen(PORT, HOST, (error) => {
+  if (error) {
+    console.error("Erro ao iniciar o servidor:", error);
+  } else {
+    console.log(`Servidor rodando em http://${HOST}:${PORT}`);
+  }
+});
 
 
 
