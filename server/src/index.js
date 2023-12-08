7ee4f0e3ba9f5e459fe4a0e55319d7356336cf98
RoqueLeto <roqueleto@gmail.com>
2023-12-07 23:13:57 -0300

    ro006

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,4 @@
-/* const express = require("express");
+const express = require("express");
 const cors = require("cors");
 app.use(cors());
 const app = express();
@@ -21,43 +21,4 @@ console.log("Server is running ok");
 //Breno é um lindão que joga duro na kofre futuro engenheiro
 //Roque barrigudo
 //Roque, o serasa me ligou hoje
-*/
-const express = require("express");
-const cors = require("cors");
-const https = require("https");
-const fs = require("fs");
-const app = express();
 
-// Configuração do CORS
-app.use(cors({
-  origin: 'http://localhost:3000' // Substitua com a URL de origem do seu frontend se necessário
-}));
-
-// Carrega variáveis de ambiente
-require("dotenv").config({ override: true });
-
-// Conexão com o banco de dados
-const startDatabase = require("./database/userDatabase");
-const createGamesTable = require("./database/gamesDatabase");
-const createGamespaceTable = require("./database/gamespaceDatabase");
-
-// Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// Rotas
-app.use(require("./routes/routes"));
-
-// Carregar as chaves e certificados SSL
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/prestecinfo.com.br/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/prestecinfo.com.br/fullchain.pem', 'utf8');
-
-const credentials = { key: privateKey, cert: certificate };
-
-// Criar servidor HTTPS
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(3000, () => {
-  console.log("HTTPS server is listening on port 3000");
-  console.log("Server is running ok");
-});
