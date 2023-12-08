1451f31225fc4c980ee5d3e147c9762a6713a28c
RoqueLeto <roqueleto@gmail.com>
2023-12-08 16:03:29 -0300

    RO023

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,4 @@
-
+/*
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -24,3 +24,42 @@ console.log("Server is running ok");
 //Breno é um lindão que joga duro na kofre futuro engenheiro
 //Roque barrigudo
 //Roque, o serasa me ligou hoje
+*/
+
+const express = require("express");
+const axios = require("axios");
+const app = express();
+require("dotenv").config({ override: true });
+const startDatabase = require("./database/userDatabase");
+const createGamesTable = require("./database/gamesDatabase");
+const createGamespaceTable = require("./database/gamespaceDatabase");
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Rotas
+app.use(require("./routes/routes"));
+
+// Exemplo de uso do Axios para fazer uma requisição GET
+app.get("/api/users/:id", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://api.example.com/users/${req.params.id}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(error.response.status).send(error.response.data);
+  }
+});
+
+// Implementação adicional
+// - Você pode adicionar interceptores de requisições e respostas para tarefas como:
+//     - Adicionar um cabeçalho de autorização a todas as requisições
+//     - Logar as requisições e respostas
+//     - Tratar erros de rede de forma centralizada
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,5 +1,4 @@
 /*
-
 // routes.js
 const { Router } = require("express");
 const router = Router();
@@ -44,28 +43,40 @@ router.get("/user/:id", getUserByID);
 // Exportando o método router para fora do arquivo
 module.exports = router;
 */
+// routes.js
+const { Router } = require("express");
+const router = Router();
+// Importando o middleware de upload
+const upload = require("../middleware/MulterMiddleware");
 
-const express = require("express");
-const axios = require("axios");
-const app = express();
-require("dotenv").config({ override: true });
-const startDatabase = require("./database/userDatabase");
-const createGamesTable = require("./database/gamesDatabase");
-const createGamespaceTable = require("./database/gamespaceDatabase");
+// Importando as funções da classe Controller
+const {
+  getUsers,
+  createUser,
+  getUserByID,
+  deleteUser,
+  updateUser,
+} = require("../controller/userController");
+const { getGames, createGame, getGameByID } = require("../controller/gameController");
+const loginUser = require("../controller/LoginController");
 
-// Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// Rotas Públicas
+router.get("/users", getUsers);
+router.post("/auth/register", createUser);
+router.delete("/users/:id", deleteUser);
+router.put("/users/:id", updateUser);
+router.post("/auth/login", loginUser);
 
-// Rotas
-app.use(require("./routes/routes"));
-
-// Exemplo de uso do Axios para fazer uma requisição GET
-app.get("/api/users/:id", async (req, res) => {
+router.get("/games", getGames);
+router.get("/game/:id", getGameByID);
+router.post("/upload", upload.single("img"), (req, res) => {
+  // Lógica para tratar o upload da imagem
+  res.json({ message: "Image uploaded successfully", filename: req.file.filename });
+});
+router.post("/create/game", async (req, res) => {
   try {
-    const response = await axios.get(
-      `https://api.example.com/users/${req.params.id}`
-    );
+    // Utilizando Axios para enviar a requisição POST
+    const response = await axios.post("/api/games", req.body);
     res.json(response.data);
   } catch (error) {
     console.error(error);
@@ -73,12 +84,18 @@ app.get("/api/users/:id", async (req, res) => {
   }
 });
 
-// Implementação adicional
-// - Você pode adicionar interceptores de requisições e respostas para tarefas como:
-//     - Adicionar um cabeçalho de autorização a todas as requisições
-//     - Logar as requisições e respostas
-//     - Tratar erros de rede de forma centralizada
-
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+// Rotas Privadas
+router.get("/user/:id", async (req, res) => {
+  try {
+    const response = await axios.get(`/api/users/${req.params.id}`);
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(error.response.status).send(error.response.data);
+  }
 });
+
+// Exportando o método router para fora do arquivo
+module.exports = router;
+
+
