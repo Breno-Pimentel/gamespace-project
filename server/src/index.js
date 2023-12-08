53ccae9c2027c35bc1dabcb0d421eecc399fec4f
RoqueLeto <roqueleto@gmail.com>
2023-12-08 00:38:03 -0300

    RO014

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,4 @@
+/*
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -20,4 +21,34 @@ console.log("Server is running ok");
 //Breno é um lindão que joga duro na kofre futuro engenheiro
 //Roque barrigudo
 //Roque, o serasa me ligou hoje
+*/
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config({ override: true });
+const startDatabase = require("./database/userDatabase");
+const createGamesTable = require("./database/gamesDatabase");
+const createGamespaceTable = require("./database/gamespaceDatabase");
+
+// Configurar o middleware CORS
+const corsOptions = {
+  origin: "http://www.prestecinfo.com.br", // Substitua pelo seu domínio HTTP
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Se você estiver usando cookies ou autenticação
+  optionsSuccessStatus: 204, // Alguns navegadores podem exigir isso
+};
+
+app.use(cors(corsOptions));
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Rotas
+app.use(require("./routes/routes"));
+
+app.listen(3000);
+console.log("Listening on port 3000 (HTTP)");
+console.log("Server is running OK");
+
 
