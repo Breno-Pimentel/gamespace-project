17304a37b02ba403a4cd4248ddbe95d601cb96e6
RoqueLeto <roqueleto@gmail.com>
2023-12-09 15:07:09 -0300

    RO043

--- server/src/database/userDatabase.js
+++ server/src/database/userDatabase.js
@@ -1,3 +1,95 @@
+const { Pool } = require("pg");
+require("dotenv").config(); // Remova a opção debug
+
+// Pool de conexão principal
+const mainPool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE1, // Alterado para DATABASE1
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+});
+
+// Script para criar a tabela de usuários
+const createTBScript = `CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(55) UNIQUE NOT NULL,
+  password VARCHAR(100) NOT NULL
+);`;
+
+// Função para criar a tabela de usuários
+const createUsersTable = async () => {
+  try {
+    const tableVerify = await mainPool.query(
+      "SELECT table_name FROM information_schema.tables WHERE table_name = 'users'"
+    );
+
+    if (tableVerify.rowCount === 0) {
+      await mainPool.query(createTBScript);
+      console.info("Table 'users' created successfully");
+    }
+  } catch (error) {
+    console.error("Error creating 'users' table:", error);
+  } finally {
+    mainPool.end(); // Encerre a conexão do pool principal
+  }
+};
+
+// Função para iniciar o banco de dados
+const startDatabase = async () => {
+  try {
+    const dataBaseVerify = await mainPool.query(
+      "SELECT datname FROM pg_catalog.pg_database WHERE datname = 'db_login_system'"
+    );
+
+    if (dataBaseVerify.rowCount === 0) {
+      await mainPool.query("CREATE DATABASE db_login_system");
+      console.info("Database 'db_login_system' created successfully");
+    }
+
+    createUsersTable(); // Chame a função para criar a tabela de usuários
+  } catch (error) {
+    console.error("Error starting database:", error);
+  } finally {
+    mainPool.end(); // Encerre a conexão do pool principal
+  }
+};
+
+// Execute a função para iniciar o banco de dados
+startDatabase();
+
+// Exporte apenas o pool principal
+module.exports = { mainPool };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 //Importando classes
 const { Pool } = require("pg");
 const path = require("path");
@@ -60,3 +152,4 @@ startDatabase();
 
 //exportando o metodo pool para fora do arquivo
 module.exports = { pool, startDatabase };
+*/
--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,44 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config({ override: true });
+const { startDatabase } = require("./database/userDatabase");
+const createGamesTable = require("./database/gamesDatabase");
+const createGamespaceTable = require("./database/gamespaceDatabase");
+const { getUploadedImagePath } = require("./middleware/MulterMiddleware");
+
+//middlewares - faz referências a funções que são passadas antes da rota
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Rotas - defina suas rotas aqui
+app.use(require("./routes/routes"));
+
+// Função assíncrona para iniciar o banco de dados e o servidor
+async function startServer() {
+  try {
+    await startDatabase();
+    await createGamesTable(); // Chame a função para criar a tabela de jogos, se necessário
+    await createGamespaceTable(); // Chame a função para criar a tabela de espaços de jogo, se necessário
+
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+}
+
+startServer();
+
+
+
+
+/*/
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config({ override: true });
 const {startDatabase} = require("./database/userDatabase");
 const createGamesTable = require("./database/gamesDatabase");
 const createGamespaceTable = require("./database/gamespaceDatabase");
@@ -20,3 +58,4 @@ console.log("Server is running ok");
 //Breno é um lindão que joga duro na kofre futuro engenheiro
 //Roque barrigudo
 //Roque, o serasa me ligou hoje
+*/
