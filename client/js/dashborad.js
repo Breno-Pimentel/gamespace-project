bd32589ba4422170f7f8bee7f17050724d5ab926
RoqueLeto <roqueleto@gmail.com>
2023-12-09 16:41:21 -0300

    RO051

--- client/js/dashborad.js
+++ client/js/dashborad.js
@@ -34,7 +34,7 @@ createGame.addEventListener("click", (e) => {
     formData.append("img", imageGame.files[0]);
 
     try {
-      const result = await fetch("http://localhost:3000/upload", {
+      const result = await fetch("http://www.prestecinfo.com.br:3000/upload", {
         method: "POST",
         body: formData,
       })
