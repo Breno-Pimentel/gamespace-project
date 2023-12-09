170f05a871c5cf208af7611e21c414297a030e55
Breno-Pimentel <brenopimentel.a.m@outlook.com>
2023-12-09 18:24:08 -0300

    Backend Gamespace

--- server/src/middleware/MulterMiddleware.js
+++ server/src/middleware/MulterMiddleware.js
@@ -21,31 +21,3 @@ function getUploadedFileName() {
 }
 
 module.exports = { upload, getUploadedFileName };
-
-
-
-
-
-// // multerMiddleware.js
-// const multer = require("multer");
-// const path = require("path");
-
-// let uploadedFileName = ''; // Variável global para armazenar o nome do arquivo
-
-// const storage = multer.diskStorage({
-//   destination: "./uploads/",
-//   filename: function (req, file, cb) {
-//     const fileName = file.fieldname + "-" + Date.now() + path.extname(file.originalname);
-//     uploadedFileName = fileName; // Salva o nome do arquivo na variável global
-//     cb(null, fileName);
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-
-// // Função que retorna o nome do arquivo
-// function getUploadedFileName() {
-//   return uploadedFileName;
-// }
-
-// module.exports = {upload, getUploadedFileName};
