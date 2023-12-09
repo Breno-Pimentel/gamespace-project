4bb14b31b6a2cfeef06ec9c0e5c7c0b1dd525629
Breno-Pimentel <brenopimentel.a.m@outlook.com>
2023-12-09 17:34:37 -0300

    breno02

--- server/src/middleware/MulterMiddleware.js
+++ server/src/middleware/MulterMiddleware.js
@@ -1,47 +1,47 @@
 
 
-const multer =require("multer");
-const path =require("path");
-const fs =require("fs");
+// const multer =require("multer");
+// const path =require("path");
+// const fs =require("fs");
 
-let uploadedImagePath = ''; // Variável para armazenar o caminho da imagem
+// let uploadedImagePath = ''; // Variável para armazenar o caminho da imagem
 
-const storage = multer.diskStorage({
-  destination: "./uploads/", // Especifique o diretório onde deseja salvar as imagens
-  filename: function (req, file, cb) {
-    const fileName = file.fieldname + "-" + Date.now() + path.extname(file.originalname);
-    cb(null, fileName);
+// const storage = multer.diskStorage({
+//   destination: "./uploads/", // Especifique o diretório onde deseja salvar as imagens
+//   filename: function (req, file, cb) {
+//     const fileName = file.fieldname + "-" + Date.now() + path.extname(file.originalname);
+//     cb(null, fileName);
 
-    // Salvar o caminho da imagem na variável
-    uploadedImagePath = path.join("./uploads/", fileName);
-    saveImagePathToText(uploadedImagePath);
-  },
-});
+//     // Salvar o caminho da imagem na variável
+//     uploadedImagePath = path.join("./uploads/", fileName);
+//     saveImagePathToText(uploadedImagePath);
+//   },
+// });
 
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
 
-async function saveImagePathToText(imagePath) {
-  const textFilePath = "./imagePaths.txt";
+// async function saveImagePathToText(imagePath) {
+//   const textFilePath = "./imagePaths.txt";
 
-  try {
-    // Adicione o caminho da imagem ao arquivo de texto
-    await fs.appendFile(textFilePath, imagePath + "\n");
-    console.log("Caminho da imagem salvo com sucesso!");
-  } catch (err) {
-    console.error("Erro ao salvar o caminho da imagem no arquivo de texto:", err);
-  }
-}
+//   try {
+//     // Adicione o caminho da imagem ao arquivo de texto
+//     await fs.appendFile(textFilePath, imagePath + "\n");
+//     console.log("Caminho da imagem salvo com sucesso!");
+//   } catch (err) {
+//     console.error("Erro ao salvar o caminho da imagem no arquivo de texto:", err);
+//   }
+// }
 
-module.exports = { upload, uploadedImagePath };
-
-
+// module.exports = { upload, uploadedImagePath };
 
 
 
 
 
 
-/*
+
+
+
 // multerMiddleware.js
 const multer = require("multer");
 const path = require("path");
@@ -65,5 +65,3 @@ function getUploadedFileName() {
 }
 
 module.exports = upload, uploadedImagePath;
-
-*/
