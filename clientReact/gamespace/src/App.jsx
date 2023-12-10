e75fa2c7015d646ab7eed0fb772e6389bb107840
RoqueLeto <roqueleto@gmail.com>
2023-12-10 14:52:19 -0300

    RO062

--- clientReact/gamespace/src/App.jsx
+++ clientReact/gamespace/src/App.jsx
@@ -1,5 +1,4 @@
-//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import { index } from "./components/index";
 import { loginPage } from "./components/loginPage";
@@ -7,12 +6,10 @@ import { loginPage } from "./components/loginPage";
 const App = () => {
   return (
     <Router>
-      <Switch>
         <Route path="/" exact component={index} />
         <Route path="/login" component={loginPage} />
         {/* Adicione mais rotas conforme necessário */}
-      </Switch>
-    </Router>
+     </Router>
   );
 };
 //Teste 
