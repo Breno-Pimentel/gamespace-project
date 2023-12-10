be611180e6410a39b91acec935cf954ca209dd3b
RoqueLeto <roqueleto@gmail.com>
2023-12-10 15:38:04 -0300

    Rocão08

--- clientReact/gamespace/src/App.jsx
+++ clientReact/gamespace/src/App.jsx
@@ -2,17 +2,17 @@ import { BrowserRouter as Routes, Route } from "react-router-dom";
 
 import { index } from "./pages/index";
 import { loginPage } from "./pages/loginPage";
-import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+//import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 const App = () => {
   return (
-    <BrowserRouter>
+ 
     <Routes>
         <Route path="/" exact component={index} />
         <Route path="/login" component={loginPage} />
         {/* Adicione mais rotas conforme necessário */}
-        </Routes>
-     </BrowserRouter>
+     </Routes>
+    
   );
 };
 //Teste 
