339a662523cf3d78f65f38de29cef53e408a3c25
RoqueLeto <roqueleto@gmail.com>
2023-12-10 16:00:35 -0300

    RO069

--- clientReact/gamespace/src/App.jsx
+++ clientReact/gamespace/src/App.jsx
@@ -1,20 +1,20 @@
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+  import { BrowserRouter as Routes, Route } from "react-router-dom";
 
-import { index } from "./pages/index";
-import { loginPage } from "./pages/loginPage";
-//import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+  import { index } from "./pages/index";
+  import { loginPage } from "./pages/loginPage";
+  //import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
-const App = () => {
-  return (
- 
-    <Routes>
-        <Route path="/" exact component={index} />
-        <Route path="/login" component={loginPage} />
-        {/* Adicione mais rotas conforme necessário */}
-     </Routes>
-    
-  );
-};
+  const App = () => {
+    return (
+  
+      <Routes>
+          <Route path="/" exact component={index} />
+          <Route path="/login" component={loginPage} />
+          {/* Adicione mais rotas conforme necessário */}
+      </Routes>
+      
+    );
+  };
 //Teste 
 
 export default App;
