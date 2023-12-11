793ea629730c76338129a768a25a484c55c28382
RoqueLeto <roqueleto@gmail.com>
2023-12-10 22:22:46 -0300

    RO081

---
+++ clientReact/gamespace/src/assets/services/apiConfig.js
@@ -0,0 +1,8 @@
+// apiConfig.js
+
+const apiConfig = {
+    baseURL: 'http://localhost:3000', // Substitua pelo endereço real do seu backend
+  };
+  
+  export default apiConfig;
+  
---
+++ clientReact/gamespace/src/components/MeuComponente.js
@@ -0,0 +1,12 @@
+
+function MeuComponente() {
+  // ...
+
+  return (
+    <div>
+      {/* Renderize os dados aqui */}
+    </div>
+  );
+}
+
+export default MeuComponente;
